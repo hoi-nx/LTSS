696297c8850d6ffad17f87beb1454dff3fb6479b
nguyenxuanhoi2903 <nguyenxuanhoi.2903@gmail.com>
2018-12-10 02:28:54 +0700

    jacobi

--- MPI/PhuongTrinhNhiet_MPI_2D_Jacobi2.c
+++ MPI/PhuongTrinhNhiet_MPI_2D_Jacobi2.c
@@ -10,6 +10,7 @@
 #define  dt      0.01
 #define  dx      0.1
 #define  D       0.1
+#define  epsilon 1.0e-2
 //==================================
 void DisplayMatrix(float *A, int row,  int col)
 {
@@ -51,7 +52,7 @@ main(int argc, char *argv[])
 {
     int i, j;
     float t; t=0;
-    int NP,rank,mc,itcnt;
+    int NP,rank,mc,step;
     MPI_Status status;
     double t1,t2;
     float c,u,d,l,r;
@@ -84,6 +85,7 @@ main(int argc, char *argv[])
     //==================================
     do
     {
+        //Truyền thông mảng Cu
         if (rank==0){
             for (j=0;j<n;j++) *(Cu+j) = 25;//*(Cs+0*n+j);
             MPI_Send(Cs+(mc-1)*n,n,MPI_FLOAT,rank+1,rank,MPI_COMM_WORLD);
@@ -93,6 +95,8 @@ main(int argc, char *argv[])
             MPI_Send(Cs+(mc-1)*n,n,MPI_FLOAT,rank+1,rank,MPI_COMM_WORLD);
             MPI_Recv(Cu,n,MPI_FLOAT,rank-1,rank-1,MPI_COMM_WORLD,&status);
         }
+        
+        //Truyền thông mảng
         if (rank==NP-1){
             for (j=0;j<n;j++) *(Cd+j) = 25; //*(Cs+(mc-1)*n+j);
             MPI_Send(Cs,n,MPI_FLOAT,rank-1,rank,MPI_COMM_WORLD);
@@ -103,7 +107,7 @@ main(int argc, char *argv[])
             MPI_Recv(Cd,n,MPI_FLOAT,rank+1,rank+1,MPI_COMM_WORLD,&status);
         }
         
-         itcnt=itcnt+1;
+         step=step+1;
         //FD(Cs,Cu,Cd,xNew,mc,rank,maxdiffnorm);
         
         maxdiffnorm=0.0;
@@ -112,31 +116,35 @@ main(int argc, char *argv[])
         for (  i = 0 ; i < mc ; i++ )
             for ( j = 0 ; j < n ; j++ )
             {
-//                if ((mc*rank+i>=(m/2-5)&&mc*rank+i<(m/2+5))&&(j>=(n/2-5)&&j<(n/2+5))||(mc*rank+i==0 && (j>=0 &&j<n))||
-//                    (mc*rank+i==m-1&&(j>=0 &&j<n))) {
-//                    *(xNew+i*n+j)=0;
-//                }
-                c = *(Cs+i*n+j);
-                u = (i==0)    ? *(Cu+j)     : *(Cs+(i-1)*n+j);
-                d = (i==mc-1) ? *(Cd+j)     : *(Cs+(i+1)*n+j);
-                l = (j==0)    ? 25 : *(Cs+i*n+j-1);
-                r = (j==n-1)  ? 25 : *(Cs+i*n+j+1);
-                *(xNew+i*n+j) = (u+d+l+r)/4.0;
+                //||(mc*rank+i==0 && (j>=0 &&j<n))||(mc*rank+i==m-1&&(j>=0 &&j<n))
+                if ((mc*rank+i>=(m/2-5)&&mc*rank+i<(m/2+5))&&(j>=(n/2-5)&&j<(n/2+5))) {
+                    *(xNew+i*n+j)=100;
+                }else{
+                
+                    c = *(Cs+i*n+j);
+                    u = (i==0)    ? *(Cu+j)     : *(Cs+(i-1)*n+j);
+                    d = (i==mc-1) ? *(Cd+j)     : *(Cs+(i+1)*n+j);
+                    l = (j==0)    ? 25 : *(Cs+i*n+j-1);
+                    r = (j==n-1)  ? 25 : *(Cs+i*n+j+1);
+                    *(xNew+i*n+j) = (u+d+l+r)/4.0;
+                    
+                
+                }
                 maxdiffnorm= MAX(maxdiffnorm,abs(*(xNew+i*n+j)- *(Cs+i*n+j)));
-               // maxdiffnorm += (*(xNew+i*n+j)- *(Cs+i*n+j))*(*(xNew+i*n+j)- *(Cs+i*n+j));
+
+               //maxdiffnorm += (*(xNew+i*n+j)- *(Cs+i*n+j))*(*(xNew+i*n+j)- *(Cs+i*n+j));
 
             }
         
         MPI_Allreduce( &maxdiffnorm, &gdiffnorm, 1, MPI_DOUBLE, MPI_MAX,MPI_COMM_WORLD );
         // gdiffnorm = sqrt( gdiffnorm );
        // if (rank == 0) printf( "At iteration %d, diff is %e\n", itcnt,gdiffnorm );
-        //
         for (  i = 0 ; i < mc ; i++ )
             for ( j = 0 ; j < n ; j++ )
                 *(Cs+i*n+j) = *(xNew+i*n+j);
         
        
-    }while(gdiffnorm>1.0e-2&& itcnt<100);
+    }while(gdiffnorm>epsilon);
     
     //==================================
     MPI_Gather(Cs,mc*n,MPI_FLOAT,C,mc*n,MPI_FLOAT,0,MPI_COMM_WORLD);
@@ -144,9 +152,12 @@ main(int argc, char *argv[])
     printf ("\t time:%f\n",(t2 - t1));
     if (rank==0)
     {
+        //printf( "--------FinishC--------\n");
+        //printf("Eps %f\n",epsilon);
         // printf( "Ma tran C:\n");
         //DisplayMatrix(C, m, n);
         Write2File(C,"result1.csv");
+        //printf( "--------End--------\n");
     }
     
     MPI_Finalize();
